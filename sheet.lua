699de12580ff8f721d87240b4bab602b02bd4b9f
Max <raranmariano@hotmail.com>
2018-02-23 17:36:26 -0300

    first build

---
+++ sheet.lua
@@ -0,0 +1,43 @@
+optionsShield =
+{
+    frames =
+    {
+        {   -- top
+            x = 0,
+            y = 0,
+            width = 25,
+            height = 25
+        },
+        {   -- sides
+            x = 50,
+            y = 0,
+            width = 25,
+            height = 25
+        }
+    }
+}
+
+options =
+{
+    frames =
+    {
+        {   -- player 1
+            x = 45,
+            y = 50,
+            width = 40,
+            height = 40
+        },
+        {   -- player 2
+            x = 90,
+            y = 55,
+            width = 40,
+            height = 40
+        },
+        {   -- player 3
+            x = 135,
+            y = 55,
+            width = 40,
+            height = 40
+        }
+    }
+}
