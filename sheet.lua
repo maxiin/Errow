cc0aa9f0e37b6847a3d67a37b39b1df9cf514f68
Max Sylver <raranmariano@hotmail.com>
2018-04-24 14:57:54 -0300

    new player sheet

--- sheet.lua
+++ sheet.lua
@@ -17,6 +17,61 @@ optionsShield =
     }
 }
 
+optionsPlayer =
+{
+    frames =
+    {
+        {   --player up 1 [1]
+            x = 0,
+            y = 0,
+            width = 40,
+            height = 40,
+        },
+        {   --player up 2 [2]
+            x = 0,
+            y = 40,
+            width = 40,
+            height = 40
+        },
+        {   --player up 3 [3]
+            x = 0,
+            y = 80,
+            width = 40,
+            height = 40
+        },
+        {   --player dead [4]
+            x = 0,
+            y = 120,
+            width = 40,
+            height = 40
+        },
+        {   --player left 1 [5]
+            x = 40,
+            y = 0,
+            width = 40,
+            height = 40
+        },
+        {   --player right 1 [6]
+            x = 40,
+            y = 40,
+            width = 40,
+            height = 40
+        },
+        {   --player left 2 [7]
+            x = 40,
+            y = 80,
+            width = 40,
+            height = 40
+        },
+        {   --player right 2 [8]
+            x = 40,
+            y = 120,
+            width = 40,
+            height = 40
+        }
+    }
+}
+
 options =
 {
     frames =
